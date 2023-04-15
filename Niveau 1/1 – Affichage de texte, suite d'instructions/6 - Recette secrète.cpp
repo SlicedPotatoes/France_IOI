d76e1cffe664ae24b3b13c42c1aef868e37b8e60
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-04-15 16:35:29 +0200

    Update 6 - Recette secrète.cpp

---
+++ Niveau 1/1 – Affichage de texte, suite d'instructions/6 - Recette secrète.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "robot.h"
+using namespace std;
+int main()
+{
+  remplir(5);
+  transferer(5, 3);
+  vider(3);
+  transferer(5, 3);
+  remplir(5);
+  transferer(5, 3);
+}
