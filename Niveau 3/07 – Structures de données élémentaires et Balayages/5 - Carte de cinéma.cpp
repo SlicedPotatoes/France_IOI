e4fba520d5b76adb0cef69bb49542e475e8a127e
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-04-22 11:56:55 +0200

    7 – Structures de données élémentaires et Balayages c++

--- Niveau 3/07 – Structures de données élémentaires et Balayages/5 - Carte de cinéma.cpp
+++ Niveau 3/07 – Structures de données élémentaires et Balayages/5 - Carte de cinéma.cpp
@@ -1,53 +1,8 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_ELEMENTS = 100 * 1000;
-int clients[MAX_ELEMENTS];
-int actualLength = 0;
-
-void showArray()
-{
-  for (int i = 0; i < actualLength; i++)
-  {
-    cout << clients[i] << '\n';
-  }
-}
-
-int findClient(int n)
-{
-  int min = 0;
-  int max = actualLength;
-
-  while (min <= max)
-  {
-    int mid = (min + max) / 2;
-    int client = clients[mid];
-    if (client > n)
-    {
-      max = mid - 1;
-    }
-    else if (client < n)
-    {
-      min = mid + 1;
-    }
-    else
-    {
-      return -1;
-    }
-  }
-  return min;
-}
-
-bool addClient(int n)
-{
-  int index = findClient(n);
-  if (index == -1)
-  {
-    return false;
-  }
-
-  return true;
-}
+const int MAX_ELEMENTS = 1000 * 1000 + 1;
+bool registre[MAX_ELEMENTS];
 
 int main()
 {
@@ -56,5 +11,14 @@ int main()
   cin >> length;
   for (int i = 0; i < length; i++)
   {
+    int element;
+    cin >> element;
+    if (registre[element])
+    {
+      cout << element;
+      return 0;
+    }
+    registre[element] = true;
   }
+  cout << -1;
 }
--- Niveau 3/07 – Structures de données élémentaires et Balayages/6 - Hydroélectricité.cpp
+++ Niveau 3/07 – Structures de données élémentaires et Balayages/6 - Hydroélectricité.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
+
+const int MAX_LENGTH = 100 * 1000;
+int indexMin = 0;
+int indexMax = 0;
+
 int main()
 {
   ios::sync_with_stdio(false);
+  int length, longueur, river[MAX_LENGTH];
+  cin >> longueur >> length;
+  int sum = 0;
+  for (int i = 0; i < longueur; i++)
+  {
+    int element;
+    cin >> element;
+    sum += element;
+    river[indexMax] = element;
+    indexMax++;
+  }
+  int maxSum = sum;
+  // cout << "DEBUG " << sum << '\n'; //DEBUG
+  for (int i = 0; i < length - longueur; i++)
+  {
+    int element;
+    cin >> element;
+    river[indexMax] = element;
+    sum = sum - river[indexMin] + element;
+    if (sum > maxSum)
+    {
+      maxSum = sum;
+    }
+    indexMin++;
+    indexMax++;
+    // cout << "DEBUG " << element << ' ' << sum << '\n'; //DEBUG
+  }
+  cout << maxSum;
 }
