75cf0711f4026fa14a85ccfb504fa4b8100a4481
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-12-24 02:39:51 +0100

    Niveau 3 --> 10 --> 8 --> c++

--- Niveau 3/10 – Bases/8 - Moyenne hexadécimale.cpp
+++ Niveau 3/10 – Bases/8 - Moyenne hexadécimale.cpp
@@ -1,7 +1,33 @@
+#include <cstring>
+#include <iomanip>
 #include <iostream>
-using namespace std;
+#include <sstream>
 
+using namespace std;
 int main()
 {
-  ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> hex >> n;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int element;
+        cin >> hex >> element;
+        sum += element;
+    }
+
+    stringstream ss;
+
+    ss << hex << sum / n;
+
+    string result = ss.str();
+
+    for (char c : result)
+    {
+        cout << (char)toupper(c);
+    }
 }
