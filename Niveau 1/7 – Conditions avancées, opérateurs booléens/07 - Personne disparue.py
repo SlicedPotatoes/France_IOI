caa49efe27a0d3a448456601f197b36b171083bc
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2024-05-25 02:20:15 +0200

    Update 07 - Personne disparue.py
    Fix Syntaxe Error
    Thanks to @OlcyX for noticing it

--- Niveau 1/7 – Conditions avancées, opérateurs booléens/07 - Personne disparue.py
+++ Niveau 1/7 – Conditions avancées, opérateurs booléens/07 - Personne disparue.py
@@ -1,9 +1,9 @@
 perRech = int(input())
 b = False
-for i in range(int(input()))
-    if int(input()) == perRech
+for i in range(int(input())):
+    if int(input()) == perRech:
         b = True
-if b
-    print(Sorti de la ville)
-else
-    print(Encore dans la ville)
+if b:
+    print("Sorti de la ville")
+else:
+    print("Encore dans la ville")
